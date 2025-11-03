730da09497b3e3eac01f195928ade13d8c0573cd
gitjip <2672259086@qq.com>
2025-11-03 12:49:37 +0800

    modify code

--- src/class042/Code04_RedPalindromeGoodStrings.cpp
+++ src/class042/Code04_RedPalindromeGoodStrings.cpp
@@ -17,14 +17,95 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
+string path;
+ll ans = 0;
+bool chk(int l, int r)
+{
+	for (int i = l; i <= r; ++i)
+	{
+		if (path[i] != path[r - i + l])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+void dfs(int x, int n)
+{
+	if (x == n)
+	{
+		int sum = 0;
+		for (int i = 0; i < n; ++i)
+		{
+			for (int j = i + 1; j < n; ++j)
+			{
+				if (sum > 1)
+					return;
+				sum += chk(i, j);
+			}
+		}
+		ans += (sum == 1);
+		return;
+	}
+	path[x] = 'r';
+	dfs(x + 1, n);
+	path[x] = 'e';
+	dfs(x + 1, n);
+	path[x] = 'd';
+	dfs(x + 1, n);
+}
 int main()
 {
 	// 打表
-	for (int i = 1; i <= 20; ++i)
-	{
-		// ...
-	}
+	// for (int i = 1; i <= 12; ++i)
+	// {
+	// 	path.resize(i);
+	// 	ans = 0;
+	// 	dfs(0, i);
+	// 	cout << i << " " << ans << "\n";
+	// }
+
+	// 验证
+	// for (int n = 1; n <= 12; ++n)
+	// {
+	// 	cout << n << " ";
+	// 	if (n == 1)
+	// 	{
+	// 		cout << "0\n";
+	// 	}
+	// 	else if (n == 2)
+	// 	{
+	// 		cout << "1\n";
+	// 	}
+	// 	else if (n == 3)
+	// 	{
+	// 		cout << "2\n";
+	// 	}
+	// 	else
+	// 	{
+	// 		cout << (n + 1) * 6 << "\n";
+	// 	}
+	// }
 
 	// 答案
+	int n;
+	cin >> n;
+	if (n == 1)
+	{
+		cout << "0\n";
+	}
+	else if (n == 2)
+	{
+		cout << "1\n";
+	}
+	else if (n == 3)
+	{
+		cout << "2\n";
+	}
+	else
+	{
+		cout << (n + 1) * 6 << "\n";
+	}
 	return 0;
 }
